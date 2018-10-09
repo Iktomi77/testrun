350a927cfc1e9ee2b8ecaa2a4ba262827a453b7e
Iktomi77 <43961270+Iktomi77@users.noreply.github.com>
2018-10-09 15:46:12 +0200

    Update testscript.R

--- testscript.R
+++ testscript.R
@@ -8,3 +8,5 @@ p1 <- hist(rnorm(500,4))
 p2 <- hist(rnorm(500,6))                     
 plot( p1, col=rgb(0,0,1,1/4), xlim=c(0,10))  
 plot( p2, col=rgb(1,0,0,1/4), xlim=c(0,10), add=T)  
+
+# interesting approach
